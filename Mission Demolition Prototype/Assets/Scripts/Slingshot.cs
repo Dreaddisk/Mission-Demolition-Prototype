365d7de876d8b29bf7c898a2e8459df8857eaed2
Dreaddisk <joey_manuel@outlook.com>
2018-09-17 22:32:27 -0400

    4.Slingshot bug fixed
    SOMEHOW THE FUCKING CAMERA UNTAGGED THE MAIN CAMERA TAG ITSELF.

--- Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
+++ Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
@@ -7,7 +7,7 @@ public class Slingshot : MonoBehaviour
 	#region Variables
 	[Header("Set in Inspector")]	
 	public GameObject PrefabProjectile;
-	public float velocityMult = 8f;	// a
+	public float velocityMult = 8f;
 
 	[Header("Set Dynamically")]
 	public GameObject launchPoint;
@@ -15,47 +15,22 @@ public class Slingshot : MonoBehaviour
 	public GameObject projectile;
 	public bool aimingMode;
 	
-	private Rigidbody projectileRigidBody;	// a
+	private Rigidbody projectileRigidBody;
 	#endregion
 
+	#region UnityMethods
 	void Awake()
 	{
 		Transform launchpointTrans = transform.Find("LaunchPoint");	// Searches for the LaunchPoint game Object.
 		launchPoint = launchpointTrans.gameObject;
 		launchPoint.SetActive(false);
-		launchPos = launchpointTrans.position;	// c
-	}
-	void OnMouseEnter()
-	{
-//		print("Slingshot:OnMouseenter");
-		launchPoint.SetActive(true);
-	}
-
-	void OnMouseExit()
-	{
-//		print("Slingshot:OnMouseExit");
-		launchPoint.SetActive(false);
-
-	}
-
-	void OnMouseDown()
-	{
-		// Player has pressed the mouse button while over Slingshot
-		aimingMode = true;
-		// Instantiate a Projectile
-		projectile = Instantiate(PrefabProjectile) as GameObject;
-		// Start it at the LaunchPoint
-		projectile.transform.position = launchPos;
-		// Set it to isKinnematic for now
-		projectile.GetComponent<Rigidbody>().isKinematic = true;
-		projectileRigidBody = projectile.GetComponent<Rigidbody>();
-		projectileRigidBody.isKinematic = true;
+		launchPos = launchpointTrans.position;
 	}
 
 	void Update()
 	{
 		// If Slingshot is not in aimingMode, don't run this code
-		if(!aimingMode) return;
+		if(!aimingMode)	return;
 
 		// Get the current mouse position in 2D screen coordinates
 		Vector3 mousePos2D = Input.mousePosition;
@@ -85,4 +60,33 @@ public class Slingshot : MonoBehaviour
 			projectile = null;
 		}
 	}
+
+	#endregion
+	
+		void OnMouseEnter()
+	{
+//		print("Slingshot:OnMouseenter");
+		launchPoint.SetActive(true);
+	}
+
+	void OnMouseExit()
+	{
+//		print("Slingshot:OnMouseExit");
+		launchPoint.SetActive(false);
+	}
+
+	void OnMouseDown()
+	{
+		// Player has pressed the mouse button while over Slingshot
+		aimingMode = true;
+		// Instantiate a Projectile
+		projectile = Instantiate(PrefabProjectile) as GameObject;
+		// Start it at the LaunchPoint
+		projectile.transform.position = launchPos;
+		// Set it to isKinnematic for now
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
+		projectileRigidBody = projectile.GetComponent<Rigidbody>();
+		projectileRigidBody.isKinematic = true;
+	}
+
 }// Main Class
